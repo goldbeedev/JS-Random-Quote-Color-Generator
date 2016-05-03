932e9201b9ba67705a5084b2d6e73416f9aae560
Justin Goldby <goldbee500@gmail.com>
2016-05-02 22:39:20 -0700

    project 1 edits
    more changes to project 1

--- js/script.js
+++ js/script.js
@@ -2,13 +2,12 @@
 // when user clicks anywhere on the page, the "makeQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-//defining variables we will need in the functions as placeholders
- 
+//defining variables
 
 var message = '';
 
  
-//setting up the print function to print the randomly selected quote
+//print function to print the randomly selected quote
  
 function print(message) {     
                 var outputDiv = document.getElementById('quote-box');
@@ -16,36 +15,8 @@ function print(message) {
 }               
  
 //quotes array containing objects for printQuote to print to the page
-
-var quotes = [
-{
-                quote: "Some people don't like change, but you need to embrace change if the alternative is disaster.",
-                source: "Elon Musk",           
-},
-{
-                quote: "Knowledge, like air, is vital to life. Like air, no one should be denied it.",
-                source: "Alan Moore",
-                citation: "V for Vendetta",
-                year: "1982"
-                
-},
-{
-                quote: "Reality is Wrong. Dreams are for real.",
-                source: "Tupac Shakur",
-},                
-{
-                quote: "Push yourself again and again.  Don't give an inch until the final buzzer sounds.",
-                source: "Larry Bird",
-                
-},
-{
-                quote: "Believing takes practice.",
-                source: "Madeleine L'engle",
-                citation: "A Wrinkle In Time",
-                year: "1963"
-}                
-];
  
+var viewedquotes = [];
 
 //a function that creates a random number between 0 and 4 to randomly select an object or index of the quotes array and return the value of it.
 
@@ -53,6 +24,7 @@ function getRandomQuote() {
                 var quoteObject = quotes[Math.floor(Math.random() * quotes.length)];
                 return quoteObject;
 }
+
 
 /*a function that prints out the random quote selected to the page, it does this by creating strings with HTML tags.
 The function also uses if conditionals to check if the objects contain the "citation" and "year" property.
@@ -68,22 +40,29 @@ function RandomColors() {
 		console.log(colors);
 }
 
+function getViewedquotes() {
+
+}
+
 function printQuote() {
-				var getRandomColors = RandomColors();
-				document.body.style.backgroundColor = getRandomColors;
+				
                 var printObject = getRandomQuote();
                 message += '<p class="quote"> ' + printObject.quote + '</p>';
                 message += '<p class="source"> ' + printObject.source + '</p>';
-                if (printObject.citation !== undefined) {
-                	message += '<span class ="citation"> ' + printObject.citation + '</span>';
-                }
-                if (printObject.year !== undefined) {
+                	if (printObject.citation !== undefined) {
+                		message += '<span class ="citation"> ' + printObject.citation + '</span>';
+                	}
+                	if (printObject.year !== undefined) {
                 	message += '<span class ="year"> ' + printObject.year + '</span>';
-                }
+                	}
                 print(message);
                 message = '';
+                	if (quotes.length > viewedquotes.length) {
+               		var pushQuote = viewedquotes.push(printObject);
+            	}
+                var getRandomColors = RandomColors();
+				document.body.style.backgroundColor = getRandomColors;
             }
-
 
 
 
